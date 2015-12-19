65d0b21ca873fc480489d8c47e8e5162aa8fafc3
dm.oliveira <dmztheone@gmail.com>
2015-12-19 20:02:32 -0200

    Added scripts in Julia for Training XGBoost + Random Forest.

---
+++ Kaggle/walmart_trip_type_classification/scripts/script_training_random_forest.jl
@@ -0,0 +1,66 @@
+#!/usr/bin/env julia
+using JLD
+using DataFrames
+using DecisionTree
+
+# Auxliary Functions
+function split_train_val(df; train_size=.85, random_state=1)
+    srand(random_state)
+    nrows = size(df, 1)
+    indexes = shuffle(collect(1:nrows))
+    train = df[indexes[1:round(Int, nrows*train_size)], :]
+    validation = df[indexes[(round(Int, nrows*train_size)+1):end], :] 
+    return train, validation
+end
+
+# Load Data
+train = readtable("../data/train_shallow_featured.tsv", separator='\t')
+test = readtable("../data/test_shallow_featured.tsv", separator='\t')
+
+train = train[:, :]
+test = test[:, :]
+
+features = [:Weekday, :Upc, :ScanCount, :DepartmentDescription, :FinelineNumber]
+label = :TripType
+
+X_train, X_val = split_train_val(train, train_size=.85, random_state=1)
+train_x = convert(Array{Float64,2}, X_train[:, features])
+train_y = convert(Array{Float64,1}, X_train[label])
+val_x = convert(Array{Float64,2}, X_val[:, features])
+val_y = convert(Array{Float64,1}, X_val[label])
+test_x = Array{Float64,2}(test[:, features])
+
+# Random Forest Parameters
+number_random_features = 5
+tree_number = 100
+subsample = .5
+
+println("""
+    Start Random Forest Traininig
+    ----------------------------
+    Parameters
+        Number Random Features: $number_random_features
+        Tree number: $tree_number
+        Sub-Sample: $subsample
+    """)
+
+tic()
+model = build_forest(train_y, train_x, 5, 100, 0.5)
+toc()
+
+
+println("\nStart Prediction")
+tic()
+pred_train = round(Int32, apply_forest(model, train_x))
+pred_val = round(Int32, apply_forest(model, val_x))
+pred_test = round(Int32, apply_forest(model, test_x))
+toc()
+
+println("\nSave Predictions")
+tic()
+writetable("../data/pred_train_rf.csv", DataFrame(pred=pred_train))
+writetable("../data/pred_val_rf.csv", DataFrame(pred=pred_val))
+writetable("../data/pred_test_rf.csv", DataFrame(pred=pred_test))
+toc()
+
+println("\nFinished Execution")
---
+++ Kaggle/walmart_trip_type_classification/scripts/script_training_xgboost.jl
@@ -0,0 +1,93 @@
+#!/usr/bin/env julia
+using JLD
+using DataFrames
+using XGBoost
+
+# Auxliary Functions
+function split_train_val(df; train_size=.85, random_state=1)
+    srand(random_state)
+    nrows = size(df, 1)
+    indexes = shuffle(collect(1:nrows))
+    train = df[indexes[1:round(Int, nrows*train_size)], :]
+    validation = df[indexes[(round(Int, nrows*train_size)+1):end], :] 
+    return train, validation
+end
+
+# Load Data
+train = readtable("../data/train_shallow_featured.tsv", separator='\t')
+test = readtable("../data/test_shallow_featured.tsv", separator='\t')
+
+train = train[:, :]
+test = test[:, :]
+
+features = [:Weekday, :Upc, :ScanCount, :DepartmentDescription, :FinelineNumber]
+label = :TripType
+
+X_train, X_val = split_train_val(train, train_size=.85, random_state=1)
+train_x = convert(Array{Float64,2}, X_train[:, features])
+train_y = convert(Array{Float64,1}, X_train[label])
+val_x = convert(Array{Float64,2}, X_val[:, features])
+val_y = convert(Array{Float64,1}, X_val[label])
+test_x = Array{Float64,2}(test[:, features])
+
+dtrain = DMatrix(train_x, label=train_y)
+dval = DMatrix(val_x, label=val_y);
+
+# XGBoost Parameters
+num_rounds = 1000
+params = Dict({"objective" => "multi:softmax",
+               "booster" => "gbtree",
+               "eta" => 0.3,
+               "max_depth" => 5,
+               "subsample" => 0.85
+              })
+
+println("""
+    Start XGBoost Traininig
+    ----------------------------
+    Parameters
+        Round Number: $num_rounds
+        Params: $params
+    """)
+
+watchlist = [(dtrain, "train"), (dval, "eval")]
+
+println("\nBase Model")
+tic()
+num_class = 39
+model = XGBoost.xgboost(dtrain, num_rounds, param=params, 
+                        num_class=num_class, watchlist=watchlist)
+toc()
+
+# println("\nPrepare to Tunning Model")
+# tic()
+# ptrain = XGBoost.predict(model, dtrain, output_margin=true)
+# pval  = XGBoost.predict(model, dval, output_margin=true)
+
+# num_rounds = 20
+# set_info(dtrain, "base_margin", ptrain)
+# set_info(dval, "base_margin", pval)
+# watchlist  = [(dtrain, "train2"), (dval, "eval2")]
+# toc()
+
+# println("\nTunning Model")
+# tic()
+# model = XGBoost.xgboost(dtrain, num_rounds, param=params, watchlist=watchlist)
+# toc()
+
+
+println("\nStart Prediction")
+tic()
+pred_train = round(Int32, XGBoost.predict(model, train_x))
+pred_val = round(Int32, XGBoost.predict(model, val_x))
+pred_test = round(Int32, XGBoost.predict(model, test_x))
+toc()
+
+println("\nSave Predictions")
+tic()
+writetable("../data/pred_train_xgb.csv", DataFrame(pred=pred_train))
+writetable("../data/pred_val_xgb.csv", DataFrame(pred=pred_val))
+writetable("../data/pred_test_xgb.csv", DataFrame(pred=pred_test))
+toc()
+
+println("\nFinished Execution")
